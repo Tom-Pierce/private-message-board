1b72bed733ca9a802af08c1645e406ecb1ad6a4b
Tom Pierce <tompierce4@gmail.com>
2023-12-07 15:41:22 +0000

    removed email from user sign up

--- controllers/index.js
+++ controllers/index.js
@@ -1,43 +1,10 @@
 const Message = require("../models/message");
 const User = require("../models/user");
 
-const session = require("express-session");
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
 
 const { body, validationResult } = require("express-validator");
-
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const user = await User.findOne({ username: username });
-      if (!user) {
-        return done(null, false, { message: "Incorrect username or password" });
-      }
-      const match = await bcrypt.compare(password, user.password);
-      if (!match) {
-        return done(null, false, { message: "Incorrect username or password" });
-      }
-      return done(null, user);
-    } catch (err) {
-      return done(err);
-    }
-  })
-);
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-});
 
 exports.index = async (req, res, next) => {
   try {
@@ -80,19 +47,6 @@ exports.sign_up_post = [
       return true;
     })
     .escape(),
-  body("email")
-    .trim()
-    .isEmail()
-    .withMessage("Invalid email provided")
-    // Checks email availability
-    .custom(async (value) => {
-      const user = await User.findOne({ email: value }).exec();
-      if (user) {
-        throw new Error("Email is already being used, please try another");
-      }
-      return true;
-    })
-    .escape(),
   body("password")
     .trim()
     // Checks if password fits the minimum requirements
@@ -126,7 +80,6 @@ exports.sign_up_post = [
           errors: errors.array(),
           user: {
             username: req.body.username,
-            email: req.body.email,
             password: req.body.password,
             password_confirmation: req.body.password_confirmation,
           },
@@ -138,7 +91,6 @@ exports.sign_up_post = [
           }
           const user = new User({
             username: req.body.username,
-            email: req.body.email,
             password: hashedPassword,
           });
           const result = await user.save();
@@ -150,3 +102,12 @@ exports.sign_up_post = [
     }
   },
 ];
+
+exports.log_in_get = (req, res, next) => {
+  res.render("log-in-form");
+};
+
+exports.log_in_post = passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/",
+});
--- models/user.js
+++ models/user.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
-  email: { type: String, required: true },
   isAdmin: { type: Boolean, required: false, default: false },
 });
 
