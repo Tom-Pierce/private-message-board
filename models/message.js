b0d27da15a624bab6b0c2095cff3b5ae2d5a908b
Tom Pierce <tompierce4@gmail.com>
2023-12-08 21:11:57 +0000

    fix bug that caused dateSent for messages to be the time of server start

--- models/message.js
+++ models/message.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const MessageSchema = new Schema({
   text: { type: String, required: true },
   user: { type: Schema.Types.ObjectId, ref: "user", required: true },
-  dateSent: { type: Date, required: true, default: Date.now() },
+  dateSent: { type: Date, required: true, default: Date.now },
   likes: { type: Number, required: true, default: 0 },
 });
 
