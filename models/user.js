8ae1015584f3990717a43bfd449065480f653fe6
Tom Pierce <tompierce4@gmail.com>
2023-12-08 11:02:10 +0000

    add a dateJoined to the user schema

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const UserSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   isAdmin: { type: Boolean, required: false, default: false },
+  dateJoined: { type: Date, required: true, default: Date.now() },
 });
 
 module.exports = mongoose.model("user", UserSchema);
