454cf3987d8ed22cc33f9c803fffd880f3937f5c
Tom Pierce <tompierce4@gmail.com>
2023-12-11 15:56:25 +0000

    style for viewing users messages

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,13 @@ const User = require("../models/user");
 
 exports.user_get = async (req, res, next) => {
   const user = await User.findOne({ username: req.params.username }).exec();
-  const userMessages = await Message.find({ user: user._id }).exec();
+  const userMessages = await Message.find({ user: user._id })
+    .sort({ dateSent: -1 })
+    .populate({
+      path: "replies",
+      populate: { path: "user", select: "username" },
+    })
+    .exec();
 
   res.render("user_view", {
     user: user,
